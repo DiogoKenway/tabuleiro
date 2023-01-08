86a4fa30e0de6c81799a733ba616fc5db4ac2518
Diogo Alves <diogodidi2022@gmail.com>
2023-01-08 17:12:44 -0300

    my version of the tabuleiro project

--- pages/tabuleiro.jsx
+++ pages/tabuleiro.jsx
@@ -1,9 +1,88 @@
-import LinhaTabuleiro from "./linhaTabuleiro";
+import Quadrado from "./quadrado"
 
 export default function Tabuleiro() {
     return (
-        <div className="wrapper">
-            <LinhaTabuleiro />                     
+        <div className="tabueiro">
+            <div className="linha">
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+            </div>            
+            <div className="linha">
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+            </div>
+            <div className="linha">
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+            </div>
+            <div className="linha">
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+            </div>
+            <div className="linha">
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+            </div>
+            <div className="linha">
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+            </div>            
+            <div className="linha">
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+            </div>
+            <div className="linha">
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+                <Quadrado black/>
+                <Quadrado white/>
+            </div>                    
         </div>
     )
 }
